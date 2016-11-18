096a7ad8de755efe43228edcfa1776fa5d6c5f7a
russelluo <rluo@movoto.com>
2016-11-18 18:22:06 +0800

    g

--- js/main.js
+++ js/main.js
@@ -199,6 +199,12 @@ require([], function (){
                 update(string[0], "");
             }
         }
-    })
+    });
+
+    if($(".instagram").length) {
+       require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+           obj.init();
+       });
+    }
 
 })
